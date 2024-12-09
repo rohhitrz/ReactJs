bd37a0e2067f539efbcf9740543f64ae42a55bda
Rohit <rohhit.rz@gmail.com>
2024-12-09 23:29:15 +0530

    commit

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,30 +1,98 @@
-import { useCallback, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback, useState, useEffect, useRef } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-const[length,setLengtgh]=useState(8);
-const[numbers,setNumbers]=useState(false);
-const[characters,setCharacters]=useState(false);
-const [Password,setPassword]=useState("");
+  const [length, setLength] = useState(8);
+  const [numbers, setNumbers] = useState(false);
+  const [characters, setCharacters] = useState(false);
+  const [Password, setPassword] = useState("alpha");
 
-const passwordGenrator=useCallback(()=>{
-  let pass="";
-  let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  if(numbers) str+= "0123456789";
+  // useRef Hook:
+  // The useRef hook is used to create a mutable reference object that persists across re-renders.
+  // It can hold a reference to a DOM element or any mutable value (e.g., a variable).
+  // The reference is stored in a variable and accessed through the `.current` property.
 
+  const passwordRef = useRef(null);
 
-},[length,numbers,characters,setPassword]) 
+  const passwordGenrator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numbers) str += "0123456789";
+    if (characters) str += "!@#$%^‡*()_+";
 
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numbers, characters, setPassword]);
 
+  const copyPassword = useCallback(() => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(Password);
+  }, [Password]);
 
+  useEffect(() => {
+    passwordGenrator();
+  }, [length, numbers, characters, passwordGenrator]);
 
   return (
     <>
-     <h1 className='text-4xl text-center'>Password Generator</h1>
+      {/* <h1 className='text-4xl text-center'>Password Generator</h1> */}
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800">
+        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={Password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            onClick={copyPassword}
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+          >
+            Copy
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label>Length:{length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numbers}
+              onChange={() => setNumbers((prev) => !prev)}
+            />
+            <label htmlFor="numbers">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={characters}
+              onChange={() => setCharacters((prev) => !prev)}
+            />
+            <label htmlFor="numberInput">Characters</label>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
